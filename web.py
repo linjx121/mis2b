1aa8403b91cf32c5dce816922e894e301ad1775a
linjx121 <cindy10211202@gmail.com>
2026-04-16 11:53:11 +0800

    爬蟲2

--- web.py
+++ web.py
@@ -35,8 +35,24 @@ def index():
     link += "<a href=/math2>次方根號計算</a><hr>"
     link += "<a href=/read>讀取Firestore資料</a><hr>"
     link += "<a href=/read2>靜宜資管老師查詢</a><hr>"
+    link += "<a href=/spider1>爬取子青老師本學期課程</a><hr>"
     return link
 
+
+@app.route("/spider1")
+def spider1():
+    R = ""
+    url = "https://www1.pu.edu.tw/~tcyang/course.html"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    #print(Data.text)
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".team-box a")
+
+    for i in result:
+        R += i.text + i.get("href") + "<br>"
+    return R
+      
 @app.route("/read2")
 def read2():
     Result = ""
@@ -50,9 +66,9 @@ def read2():
             Result += str(teacher) + "<br>"   
 
     if Result == "":
-        Result = "抱歉，查無此關鍵字姓名之老師資料"
-    return Result
-    return render_template("read2.html")
+            Result = "抱歉，查無此關鍵字姓名之老師資料"
+    else:
+        return render_template("read2.html")
 
 @app.route("/read")
 def read():
