17abaef9cb1c857b8169092b32e1f7be45b19434
linjx121 <cindy10211202@gmail.com>
2026-04-23 12:06:11 +0800

    0423

---
+++ py/s2.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://mis2b-nine.vercel.app/me"
+Data = requests.get(url)
+Data.encoding = "utf-8"
+#print(Data.text)
+sp = BeautifulSoup(Data.text, "html.parser")
+result=sp.find("td")
+for item in result:
+	print(item.text)
+	print()
